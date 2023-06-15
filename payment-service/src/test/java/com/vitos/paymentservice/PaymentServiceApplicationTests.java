8acd5ac15ef914e13ee27f51fb0d38593ec60cd2
yavindu <yavindu@gmail.com>
2023-06-15 17:35:27 +0530

    add order-service and payment-service

---
+++ payment-service/src/test/java/com/vitos/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitos.paymentserivice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
