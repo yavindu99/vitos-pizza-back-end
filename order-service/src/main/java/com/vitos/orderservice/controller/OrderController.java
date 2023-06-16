3702629b31ff0f8261f3d0047132166f02bbd959
yavindu <yavindu@gmail.com>
2023-06-16 07:55:06 +0530

    add circuit breaker

--- order-service/src/main/java/com/vitos/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/vitos/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.vitos.orderservice.controller;
 import com.vitos.orderservice.dto.OrderRequest;
 import com.vitos.orderservice.dto.OrderResponse;
 import com.vitos.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,16 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
     private final OrderService orderService;
     @PostMapping
+    @CircuitBreaker(name = "payment", fallbackMethod = "fallbackMethod")
     public ResponseEntity<?> placeOrder(@RequestBody OrderRequest orderRequest){
 
         OrderResponse order = orderService.placeOrder(orderRequest);
 
         return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return new ResponseEntity<>("Please try later", HttpStatus.REQUEST_TIMEOUT);
     }
 
 }
